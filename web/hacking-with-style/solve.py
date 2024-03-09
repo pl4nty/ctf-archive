b84b5747f22862c32b6f0c796d2b735ecc963a1e
Tom Plant <tom@tplant.com.au>
2024-03-09 03:17:15 +0000

    chals: sleep before solving hacking-with-style
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- web/hacking-with-style/solve.py
+++ web/hacking-with-style/solve.py
@@ -2,12 +2,14 @@ import argparse
 import os
 import requests
 import subprocess
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--connection-info', default='http://localhost:8080')
 args = parser.parse_args()
 
 subprocess.run(["docker", "compose", "up", "--detach"], cwd=os.path.dirname(__file__))
+time.sleep(5)
 
 # not actually a solve
 requests.get(args.connection_info)
